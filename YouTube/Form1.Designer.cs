5e6eee044b54599cda6d7fb074f50610d9eefd70
choesad <choesad0808@gmail.com>
2017-05-19 15:46:28 +0800

    inint commit

---
+++ YouTube/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace YouTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Url = new System.Windows.Forms.TextBox();
+            this.textBox_Comment = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Username = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video Url";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Comments";
+            // 
+            // textBox_Url
+            // 
+            this.textBox_Url.Location = new System.Drawing.Point(80, 34);
+            this.textBox_Url.Name = "textBox_Url";
+            this.textBox_Url.Size = new System.Drawing.Size(296, 20);
+            this.textBox_Url.TabIndex = 2;
+            // 
+            // textBox_Comment
+            // 
+            this.textBox_Comment.Location = new System.Drawing.Point(80, 79);
+            this.textBox_Comment.Multiline = true;
+            this.textBox_Comment.Name = "textBox_Comment";
+            this.textBox_Comment.Size = new System.Drawing.Size(296, 108);
+            this.textBox_Comment.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Passsword";
+            // 
+            // textBox_Username
+            // 
+            this.textBox_Username.Location = new System.Drawing.Point(80, 211);
+            this.textBox_Username.Name = "textBox_Username";
+            this.textBox_Username.Size = new System.Drawing.Size(110, 20);
+            this.textBox_Username.TabIndex = 6;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(273, 211);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(103, 20);
+            this.textBox_Password.TabIndex = 7;
+            this.textBox_Password.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 280);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.textBox_Username);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Comment);
+            this.Controls.Add(this.textBox_Url);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Youtube";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Url;
+        private System.Windows.Forms.TextBox textBox_Comment;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Username;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ YouTube/Form1.cs
@@ -0,0 +1,141 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebRequest request = null;
+            WebResponse response = null;
+            Stream resStream = null;
+            StreamReader resReader = null;
+            Boolean programRun = false;
+            try
+            {
+                string uristring = "http://venusgod.eu.pn/check.html";
+                request = WebRequest.Create(uristring.Trim());
+                response = request.GetResponse();
+                resStream = response.GetResponseStream();
+                resReader = new StreamReader(resStream);
+                string resstring = resReader.ReadToEnd();
+                string keyString = "youtube=ture";
+                if (resstring.Contains(keyString))
+                {
+                    programRun = true;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                if (resReader != null) resReader.Close();
+                if (response != null) response.Close();
+            }
+
+
+            if (programRun == true)
+            {
+                System.Threading.Thread.Sleep(2000);
+                string url = textBox_Url.Text;
+                string comments = textBox_Comment.Text;
+                string username = textBox_Username.Text;
+                string password = textBox_Password.Text;
+
+                var driverService = ChromeDriverService.CreateDefaultService();
+                driverService.HideCommandPromptWindow = true;
+                ChromeOptions option = new ChromeOptions();
+                option.AddArguments("disable-infobars");               //disable test automation message
+                option.AddArguments("--disable-notifications");        //disable notifications
+                option.AddArguments("--disable-web-security");         //disable save password windows
+                option.AddUserProfilePreference("credentials_enable_service", false);
+                option.AddUserProfilePreference("disable-popup-blocking", "true");
+                //option.AddArgument("--window-position=-32000,-32000");
+                ChromeDriver navigator = new ChromeDriver(driverService, option);
+
+
+                //InternetExplorerDriver navigator = new InternetExplorerDriver();
+
+                try
+                {
+                    navigator.Navigate().GoToUrl(url);
+                    System.Threading.Thread.Sleep(5000);
+
+                    navigator.FindElement(By.XPath("//button[@class='yt-uix-button yt-uix-button-size-default yt-uix-button-primary']")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                    System.Threading.Thread.Sleep(5000);
+                    navigator.FindElement(By.Id("Email")).SendKeys(username);
+                    System.Threading.Thread.Sleep(500);
+                    navigator.FindElement(By.Id("next")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                    System.Threading.Thread.Sleep(3000);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                try
+                {
+                    navigator.FindElement(By.Id("Passwd")).SendKeys(password);
+                    System.Threading.Thread.Sleep(500);
+                    navigator.FindElement(By.Id("signIn")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                    System.Threading.Thread.Sleep(10000);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                try
+                {
+                    navigator.FindElement(By.XPath("//div[@class='comment-simplebox-renderer-collapsed comment-simplebox-trigger']")).Click();
+                    System.Threading.Thread.Sleep(1000);
+
+                    // method1
+                    // navigator.FindElement(By.XPath("//div[@class='comment-simplebox-text']")).SendKeys(comments);
+                    // method2
+                    // SendKeys.SendWait(comments);
+                    // method3
+                    IJavaScriptExecutor js = navigator as IJavaScriptExecutor;
+                    js.ExecuteScript("document.getElementsByClassName('comment-simplebox-text')[0].innerHTML = '"+ comments + "';");
+                    System.Threading.Thread.Sleep(1000);
+                    js.ExecuteScript("document.getElementsByClassName('yt-uix-button yt-uix-button-size-default yt-uix-button-primary yt-uix-button-empty comment-simplebox-submit yt-uix-sessionlink')[0].disabled  = false;");
+                    System.Threading.Thread.Sleep(1000);
+                    navigator.FindElement(By.XPath("//button[@class='yt-uix-button yt-uix-button-size-default yt-uix-button-primary yt-uix-button-empty comment-simplebox-submit yt-uix-sessionlink']")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                    System.Threading.Thread.Sleep(5000);
+                }
+
+                catch (Exception ex)
+                {
+                }
+
+                
+                navigator.Close();
+                MessageBox.Show("Done!!!");
+            }
+            else
+                MessageBox.Show("Because Youtube website changed, the program can't run!");
+
+        }
+    }
+}
